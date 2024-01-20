fff54707184ab5702f6ff9fd22aa77f23a7b7a5b
BoopluK2 <110056464+BoopluK2@users.noreply.github.com>
2024-01-20 13:20:37 +0500

    use

--- server/index.js
+++ server/index.js
@@ -16,9 +16,19 @@ coonectDatebase();
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
     next();
-});
+  });
+  
+  
 
 
 
